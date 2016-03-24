40149d1f191cbea3c7c729b634d177e154246cd9
sirichandana24 <sirichandana24@missionrnd.com>
2016-03-24 20:11:08 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -26,5 +26,13 @@
 
 int get_steps(int s)
 {
-	return 0;
+	if (s == 0)
+		return 0;
+	if (s == 1)
+		return 1;
+	if (s == 2)
+		return 2;
+	if (s >= 3)
+	return	get_steps(s - 1) + get_steps(s - 2);
+		
 }
